59fe8d0e139056700701eb79e0c9c7a306b926a0
hamad ali <hamad.ali.dev@gmail.com>
2025-11-02 23:03:47 +0500

    remoev text in login

--- client/src/views/login/index.vue
+++ client/src/views/login/index.vue
@@ -43,11 +43,6 @@
 
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
-      <div class="tips">
-        <span style="margin-right:20px;">email: admin@recipe-book.com</span>
-        <span> password: password123</span>
-      </div>
-
       <div class="register-link">
         <span>Don't have an account? </span>
         <router-link to="/register" class="link">Register here</router-link>
